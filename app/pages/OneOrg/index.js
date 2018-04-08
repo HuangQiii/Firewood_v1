dc8edadff13565de8c85eab8139d188e45264a3a
HuangQiii <598260403@qq.com>
2018-04-08 22:47:43 +0800

    [FIX] fix one org

--- app/pages/OneOrg/index.js
+++ app/pages/OneOrg/index.js
@@ -10,24 +10,24 @@ const { width, height } = Dimensions.get('window');
 
 const DATA = [
     {
-        id:1,
-        org:'光华园',
-        des:'我们要在哪里哪里举办什么什么了，丰厚奖品等你来拿，具体链接请戳。。。',
-        time:'2018.3.11 14:00',
-        ava:'https://avatars2.githubusercontent.com/u/10620750?s=96&v=4',
-        shareAva:'https://avatars2.githubusercontent.com/u/14921202?s=96&v=4',
-        shareTitle:'影像大赛',
-        shareTime:'2011.3.3 15：30',
+        id: 1,
+        org: '光华园',
+        des: '我们要在哪里哪里举办什么什么了，丰厚奖品等你来拿，具体链接请戳。。。',
+        time: '2018.3.11 14:00',
+        ava: 'https://avatars2.githubusercontent.com/u/10620750?s=96&v=4',
+        shareAva: 'https://avatars2.githubusercontent.com/u/14921202?s=96&v=4',
+        shareTitle: '影像大赛',
+        shareTime: '2011.3.3 15：30',
     },
     {
-        id:2,
-        org:'光华园',
-        des:'PPT培训',
-        time:'2018.3.11 14:00',
-        ava:'https://avatars2.githubusercontent.com/u/10620750?s=96&v=4',
-        shareAva:'https://avatars0.githubusercontent.com/u/2833137?s=96&v=4',
-        shareTitle:'PPT讲座',
-        shareTime:'2011.3.3 15：30',
+        id: 2,
+        org: '光华园',
+        des: 'PPT培训',
+        time: '2018.3.11 14:00',
+        ava: 'https://avatars2.githubusercontent.com/u/10620750?s=96&v=4',
+        shareAva: 'https://avatars0.githubusercontent.com/u/2833137?s=96&v=4',
+        shareTitle: 'PPT讲座',
+        shareTime: '2011.3.3 15：30',
     },
 ];
 
@@ -39,67 +39,76 @@ class OneOrg extends Component {
         ),
     });
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            dataSourceCommit:new ListView.DataSource({
-                rowHasChanged:(row1,row2)=>row1!==row2,
+        this.state = {
+            dataSourceCommit: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
             }),
         }
     }
 
     componentDidMount() {
         this.setState({
-            dataSourceCommit:this.state.dataSourceCommit.cloneWithRows(DATA),
+            dataSourceCommit: this.state.dataSourceCommit.cloneWithRows(DATA),
         });
     }
 
     renderCommit = (item) => {
         return (
             <View style={styles.trends}>
-                    <View style={styles.trendsHead}>
-                        <Text style={{fontSize:32}}>22</Text>
-                        <Text>3月</Text>
-                    </View>
-                    <View style={styles.trendsRight}>
-                        <View style={styles.trendsTitleBlock}>
-                            <Text style={styles.trendsTitleLink}>
-                                {item.org}
+                <View style={styles.trendsHead}>
+                    <Text style={{ fontSize: 32 }}>22</Text>
+                    <Text>3月</Text>
+                </View>
+                <View style={styles.trendsRight}>
+                    <View style={styles.trendsTitleBlock}>
+                        <Text style={styles.trendsTitleLink}>
+                            {item.org}
+                        </Text>
+                        <Text style={styles.trendsTitleNormal}>
+                            分享活动
                             </Text>
-                            <Text style={styles.trendsTitleNormal}>
-                                分享活动
+                    </View>
+                    <Text style={styles.trendsTime}>
+                        {item.time}
+                    </Text>
+                    <Text style={styles.trendsDes}>
+                        {item.des}
+                    </Text>
+                    <TouchableOpacity
+                        onPress={() => this.props.navigation.navigate('OneActivity')}
+                    >
+                        <View style={styles.trendsShare}>
+                            <View>
+                                <Image
+                                    resizeMode='stretch'
+                                    style={{ width: 40, height: 40, marginRight: 10, }}
+                                    source={{ uri: item.shareAva }}
+                                />
+                            </View>
+                            <View style={{ justifyContent: 'center' }}>
+                                <Text style={styles.trendsShareTitle}>{item.shareTitle}</Text>
+                                <Text style={styles.trendsShareTip}>{item.shareTime}</Text>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                    <View style={styles.trendsToolBlock}>
+                        <View style={styles.trendToolLi}>
+                            <Icon name="md-text" size={15} />
+                            <Text style={styles.trendsTool}>
+                                0
                             </Text>
                         </View>
-                        <Text style={styles.trendsTime}>
-                            {item.time}
-                        </Text>
-                        <Text style={styles.trendsDes}>
-                            {item.des}
-                        </Text>
-                        <TouchableOpacity
-                            onPress={() => this.props.navigation.navigate('OneActivity')}
-                        >
-                            <View style={styles.trendsShare}>
-                                <View>
-                                    <Image
-                                        resizeMode='stretch'
-                                        style={{ width: 40, height: 40, marginRight:10,}}
-                                        source={{uri:item.shareAva}}
-                                    />
-                                </View>
-                                <View style={{justifyContent:'center'}}>
-                                    <Text style={styles.trendsShareTitle}>{item.shareTitle}</Text>
-                                    <Text style={styles.trendsShareTip}>{item.shareTime}</Text>
-                                </View>
-                            </View>
-                        </TouchableOpacity>
-                        <View style={styles.trendsToolBlock}>
+                        <View style={styles.trendToolLi}>
+                            <Icon name="md-thumbs-up" size={15} />
                             <Text style={styles.trendsTool}>
-                                点赞
+                                0
                             </Text>
                         </View>
                     </View>
                 </View>
+            </View>
         );
     }
 
@@ -108,24 +117,28 @@ class OneOrg extends Component {
         const { navigate } = this.props.navigation;
         return (
             <View style={styles.container}>
-                <View style={{flex:1}}>
+                <View style={{ flex: 1 }}>
                     <ScrollView>
                         <View style={styles.header}>
-                            <View style={{width:80,height:80,borderRadius:40,overflow:'hidden'}}>
+                            <View style={{ width: 80, height: 80, borderRadius: 40, overflow: 'hidden' }}>
                                 <Image
-                                    style={{width:80,height:80,}}
-                                    resizeMode = 'cover'
-                                    source={{uri:'https://avatars2.githubusercontent.com/u/10620750?s=96&v=4'}}
+                                    style={{ width: 80, height: 80, }}
+                                    resizeMode='cover'
+                                    source={{ uri: 'https://avatars2.githubusercontent.com/u/10620750?s=96&v=4' }}
                                 />
                             </View>
                             <View style={styles.headerText}>
                                 <Text style={styles.headerName}>光华园</Text>
                             </View>
                         </View>
-                        <View style={[styles.section,{justifyContent:'center',alignItems:'center'}]}>
-                            <View style={{paddingHorizontal:10,paddingVertical:5,backgroundColor:'#477aac',borderRadius:4,}}>
-                                <Text style={{fontSize:14,color:'#fff'}}>给他留言</Text>
-                            </View>
+                        <View style={[styles.section, { justifyContent: 'center', alignItems: 'center' }]}>
+                            <TouchableOpacity
+                                onPress={() => navigate('Write')}
+                            >
+                                <View style={{ paddingHorizontal: 10, paddingVertical: 5, backgroundColor: '#477aac', borderRadius: 4, }}>
+                                    <Text style={{ fontSize: 14, color: '#fff' }}>给他留言</Text>
+                                </View>
+                            </TouchableOpacity>
                         </View>
                         <View style={styles.section}>
                             <View style={styles.titleBlock}>
@@ -147,7 +160,7 @@ class OneOrg extends Component {
                             <View style={styles.titleBlock}>
                                 <Text style={styles.title}>活动简介</Text>
                             </View>
-                            <Text>    假装我是简介，假装我是简介，假装我是简介，假装我是简介，假装我是简介，假装我是简介，假装我是简介，假装我是简介，假装我是简介，假装我是简介，假装我是简介，假装我是简介，假装我是简介，假装我是简介，假装我是简介，假装我是简介，假装我是简介。</Text>
+                            <Text>        西班牙有一位乡绅，酷爱骑士小说，终日捧读，手不释卷，最后读昏了头脑，竟异想天开，要去做一名游侠骑士，锄强扶弱，行侠丈义，闯荡天下。他说服村上一位农夫做他的侍从，乘马持予，踏上冒险之途。他屡战屡败，屡败屡战，坚持自己的理想，直到一命归西。</Text>
                         </View>
                         <View style={styles.section}>
                             <View style={styles.titleBlock}>
--- app/pages/OneOrg/style.js
+++ app/pages/OneOrg/style.js
@@ -1,7 +1,7 @@
-import { StyleSheet,Dimensions } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import { PALETTA, SIZE } from '../../style/Common';
 
-const {width,height} = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
 var styles = StyleSheet.create({
     container: {
@@ -9,191 +9,197 @@ var styles = StyleSheet.create({
         backgroundColor: PALETTA.LIGHT_BG,
     },
     header: {
-        width:width,
-        height:200,
-        alignItems:'center',
-        justifyContent:'center',
-        backgroundColor:PALETTA.LOGHT_SPLIT_BG,
+        width: width,
+        height: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: PALETTA.LOGHT_SPLIT_BG,
     },
-    headerText:{
-        marginTop:20,
+    headerText: {
+        marginTop: 20,
     },
-    headerName:{
-        fontSize:24,
-        color:PALETTA.VITAL_TITLE,
+    headerName: {
+        fontSize: 24,
+        color: PALETTA.VITAL_TITLE,
     },
-    activityName:{
-        fontSize:24,
-        color:'#fff',
-        marginBottom:10,
+    activityName: {
+        fontSize: 24,
+        color: '#fff',
+        marginBottom: 10,
     },
-    activityTip:{
-        fontSize:SIZE.LIGHT_TIP,
-        color:'#fff',
+    activityTip: {
+        fontSize: SIZE.LIGHT_TIP,
+        color: '#fff',
     },
     section: {
-        paddingHorizontal:20,
-        paddingVertical:15,
+        paddingHorizontal: 20,
+        paddingVertical: 15,
     },
-    titleBlock:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
-        marginBottom:15,
+    titleBlock: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 15,
     },
     title: {
-        fontSize:SIZE.VITAL_NAV_TITLE,
-        color:PALETTA.VITAL_TITLE,
+        fontSize: SIZE.VITAL_NAV_TITLE,
+        color: PALETTA.VITAL_TITLE,
     },
     more: {
-        fontSize:SIZE.LIGHT_TIP,
-        color:PALETTA.NORMAL_999,
+        fontSize: SIZE.LIGHT_TIP,
+        color: PALETTA.NORMAL_999,
     },
     body: {
-        marginTop:15,
+        marginTop: 15,
     },
-    orgHeader:{
-        width:40,
-        height:40,
-        marginRight:20,
-        borderRadius:20,
-        overflow:'hidden',
+    orgHeader: {
+        width: 40,
+        height: 40,
+        marginRight: 20,
+        borderRadius: 20,
+        overflow: 'hidden',
     },
-    orgName:{
-        fontSize:SIZE.NORMAL_CONTENT,
-        color:PALETTA.VITAL_TITLE,
+    orgName: {
+        fontSize: SIZE.NORMAL_CONTENT,
+        color: PALETTA.VITAL_TITLE,
     },
-    trendsLabel:{
-      flexDirection:'row',
-      flexWrap:'wrap',
-      marginVertical:3,
+    trendsLabel: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginVertical: 3,
     },
-    trendsLabelWrapper:{
-      paddingHorizontal:5,
-      paddingVertical:3,
-      backgroundColor:PALETTA.LOGHT_SPLIT_BG,
-      borderRadius:4,
-      marginRight:5,
-      alignItems:'center',
-      justifyContent:'center',
+    trendsLabelWrapper: {
+        paddingHorizontal: 5,
+        paddingVertical: 3,
+        backgroundColor: PALETTA.LOGHT_SPLIT_BG,
+        borderRadius: 4,
+        marginRight: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-    trendsLabelWord:{
-      fontSize:SIZE.LIGHT_TIP,
-      color:PALETTA.VITAL_TITLE,
+    trendsLabelWord: {
+        fontSize: SIZE.LIGHT_TIP,
+        color: PALETTA.VITAL_TITLE,
     },
-    
+
     commit: {
-        padding:10,
-        marginBottom:10,
-        flexDirection:'row',
-        height:80,
+        padding: 10,
+        marginBottom: 10,
+        flexDirection: 'row',
+        height: 80,
     },
-    commitHeader:{
-        width:40,
-        height:40,
-        borderRadius:20,
-        overflow:'hidden',
+    commitHeader: {
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        overflow: 'hidden',
     },
-    commitMessage:{
-        flex:1,
-        paddingLeft:10,
-        justifyContent:'space-between',
+    commitMessage: {
+        flex: 1,
+        paddingLeft: 10,
+        justifyContent: 'space-between',
     },
-    commitName:{
-        fontSize:SIZE.NORMAL_TITLE,
-        color:PALETTA.VITAL_LINK,
+    commitName: {
+        fontSize: SIZE.NORMAL_TITLE,
+        color: PALETTA.VITAL_LINK,
     },
-    commitWord:{
-        fontSize:SIZE.NORMAL_CONTENT,
-        color:PALETTA.VITAL_TITLE,
+    commitWord: {
+        fontSize: SIZE.NORMAL_CONTENT,
+        color: PALETTA.VITAL_TITLE,
     },
-    commitTime:{
-        fontSize:SIZE.LIGHT_TIME,
-        color:PALETTA.NORMAL_b2,
+    commitTime: {
+        fontSize: SIZE.LIGHT_TIME,
+        color: PALETTA.NORMAL_b2,
     },
     btnBlock: {
-        flex:1,
-        height:40,
-        justifyContent:'center',
-        alignItems:'center',
+        flex: 1,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     trends: {
-      paddingVertical:15,
-      flexDirection:'row',
+        paddingVertical: 15,
+        flexDirection: 'row',
     },
-    trendsHead:{
-      width:60,
-    //   height:40,
-    //   borderRadius:20,
-    //   overflow:'hidden',
-        flexDirection:'row',
+    trendsHead: {
+        width: 60,
+        //   height:40,
+        //   borderRadius:20,
+        //   overflow:'hidden',
+        flexDirection: 'row',
     },
-    trendsRight:{
-      flex:1,
-      paddingLeft:15,
+    trendsRight: {
+        flex: 1,
+        paddingLeft: 15,
     },
-    trendsTitleBlock:{
-      flexDirection:'row',
-      alignItems:'center',
+    trendsTitleBlock: {
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     trendsTitleLink: {
-      fontSize:SIZE.NORMAL_TITLE,
-      color:PALETTA.VITAL_LINK,
+        fontSize: SIZE.NORMAL_TITLE,
+        color: PALETTA.VITAL_LINK,
     },
-    trendsTitleNormal:{
-      fontSize:SIZE.NORMAL_TITLE,
-      color:PALETTA.NORMAL_666,
-      marginLeft:3,
+    trendsTitleNormal: {
+        fontSize: SIZE.NORMAL_TITLE,
+        color: PALETTA.NORMAL_666,
+        marginLeft: 3,
     },
-    trendsTime:{
-      fontSize:SIZE.LIGHT_TIME,
-      color:PALETTA.NORMAL_b2,
-      marginTop:3,
+    trendsTime: {
+        fontSize: SIZE.LIGHT_TIME,
+        color: PALETTA.NORMAL_b2,
+        marginTop: 3,
     },
-    trendsDes:{
-      fontSize:SIZE.NORMAL_CONTENT,
-      color:PALETTA.VITAL_TITLE,
-      marginTop:5,
+    trendsDes: {
+        fontSize: SIZE.NORMAL_CONTENT,
+        color: PALETTA.VITAL_TITLE,
+        marginTop: 5,
     },
-    trendsShare:{
-      flexDirection:'row',
-      marginTop:5,
-      backgroundColor:PALETTA.LOGHT_SPLIT_BG,
-      padding:5,
+    trendsShare: {
+        flexDirection: 'row',
+        marginTop: 5,
+        backgroundColor: PALETTA.LOGHT_SPLIT_BG,
+        padding: 5,
     },
-    trendsShareTitle:{
-      fontSize:SIZE.LIGHT_TIP,
-      color:PALETTA.VITAL_TITLE,
+    trendsShareTitle: {
+        fontSize: SIZE.LIGHT_TIP,
+        color: PALETTA.VITAL_TITLE,
     },
-    trendsShareTip:{
-      fontSize:SIZE.LIGHT_SMALL_TIP,
-      color:PALETTA.NORMAL_b2,
+    trendsShareTip: {
+        fontSize: SIZE.LIGHT_SMALL_TIP,
+        color: PALETTA.NORMAL_b2,
     },
-    trendsToolBlock:{
-      alignItems:'flex-end',
-      marginTop:3,
+    bottomToolbar: {
+        height: 50,
+        backgroundColor: '#fff',
+        borderTopWidth: 1,
+        borderTopColor: PALETTA.LOGHT_SPLIT,
+        paddingVertical: 5,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
     },
-    trendsTool:{
-      fontSize:SIZE.LIGHT_TIP,
-      color:PALETTA.NORMAL_666,
-    },
-    bottomToolbar:{
-        height:50,
-        backgroundColor:'#fff',
-        borderTopWidth:1,
-        borderTopColor:PALETTA.LOGHT_SPLIT,
-        paddingVertical:5,
-        flexDirection:'row',
-        alignItems:'center',
-        justifyContent:'flex-end',
-    },
-    bottomToolbarButton:{
-        width:width/2,
+    bottomToolbarButton: {
+        width: width / 2,
         // justifyContent:'center',
-        alignItems:'center',
-        borderLeftColor:PALETTA.LOGHT_SPLIT,
-        borderLeftWidth:1,
-
+        alignItems: 'center',
+        borderLeftColor: PALETTA.LOGHT_SPLIT,
+        borderLeftWidth: 1,
+    },
+    trendsToolBlock: {
+        flexDirection: 'row',
+        marginTop: 5,
+        justifyContent: 'flex-end',
+    },
+    trendToolLi: {
+        flexDirection: 'row',
+        marginLeft: 15,
+        alignItems: 'center',
+    },
+    trendsTool: {
+        fontSize: SIZE.LIGHT_TIP,
+        color: PALETTA.NORMAL_666,
+        marginLeft: 5,
     },
 });
 
