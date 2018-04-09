db243cd9f7d3bebadef2835e27dae10a71dbb896
HuangQiii <598260403@qq.com>
2018-04-09 23:17:00 +0800

    [FIX] fix circle

--- app/pages/Circle/index.js
+++ app/pages/Circle/index.js
@@ -1,53 +1,50 @@
 import React, { Component } from 'react';
-import { ScrollView, AppState, View, Dimensions, StyleSheet, Text, Image, TouchableOpacity, ListView, TouchableHighlight, DeviceEventEmitter, NetInfo, NativeModules,FlatList } from 'react-native';
+import { ScrollView, View, Text, Image, TouchableOpacity, ListView } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { connect } from 'react-redux';
 import styles from './style';
-import ScrollableTabView, {ScrollableTabBar, } from 'react-native-scrollable-tab-view';
-import IconBlock from '../../components/IconBlock';
 
-const { width, height } = Dimensions.get('window');
 const DATA = [
     {
-        id:1,
-        org:'光华园',
-        des:'我们要在哪里哪里举办什么什么了，丰厚奖品等你来拿，具体链接请戳。。。',
-        time:'2018.3.11 14:00',
-        ava:'https://avatars1.githubusercontent.com/u/19502297?s=96&v=4',
-        shareAva:'https://avatars2.githubusercontent.com/u/14921202?s=96&v=4',
-        shareTitle:'影像大赛',
-        shareTime:'2011.3.3 15：30',
+        id: 1,
+        org: '光华园',
+        des: '我们要在哪里哪里举办什么什么了，丰厚奖品等你来拿，具体链接请戳。。。',
+        time: '2018.3.11 14:00',
+        ava: 'https://avatars1.githubusercontent.com/u/19502297?s=96&v=4',
+        shareAva: 'https://avatars2.githubusercontent.com/u/14921202?s=96&v=4',
+        shareTitle: '影像大赛',
+        shareTime: '2011.3.3 15：30',
     },
     {
-        id:2,
-        org:'GLOBAL软件部落',
-        des:'PPT培训',
-        time:'2018.3.11 14:00',
-        ava:'https://avatars3.githubusercontent.com/u/4016839?s=96&v=4',
-        shareAva:'https://avatars0.githubusercontent.com/u/2833137?s=96&v=4',
-        shareTitle:'PPT讲座',
-        shareTime:'2011.3.3 15：30',
+        id: 2,
+        org: 'GLOBAL软件部落',
+        des: 'PPT培训',
+        time: '2018.3.11 14:00',
+        ava: 'https://avatars3.githubusercontent.com/u/4016839?s=96&v=4',
+        shareAva: 'https://avatars0.githubusercontent.com/u/2833137?s=96&v=4',
+        shareTitle: 'PPT讲座',
+        shareTime: '2011.3.3 15：30',
     },
 ];
 
-class Home extends Component {
+class Circle extends Component {
 
     static navigationOptions = ({ navigation }) => ({
-        title:'朋友圈',
+        title: '朋友圈',
     });
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            dataSource:new ListView.DataSource({
-                rowHasChanged:(row1,row2)=>row1!==row2,
+        this.state = {
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
             }),
         }
     }
 
     componentDidMount() {
         this.setState({
-            dataSource:this.state.dataSource.cloneWithRows(DATA),
+            dataSource: this.state.dataSource.cloneWithRows(DATA),
         });
     }
 
@@ -137,4 +134,4 @@ export default connect(
     }),
     (dispatch) => ({
     })
-)(Home)
+)(Circle)
